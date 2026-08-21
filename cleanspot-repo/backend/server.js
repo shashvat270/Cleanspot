c36043f4baa63ed8ceb8ffe16bba9db60fbf38bb
shashvat270 <shashvatkawali27@gmail.com>
2026-08-21 23:15:31 +0530

    Update server.js

--- cleanspot-repo/backend/server.js
+++ cleanspot-repo/backend/server.js
@@ -12,7 +12,9 @@ const driveRoutes = require("./routes/drives");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://cleanspot-seven.vercel.app",
+  }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
